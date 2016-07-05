445d80f2ecd8a57fc671eccd729ed851ec06fe9e
SuthigaR <suthigafdo@gmail.com>
2016-07-05 18:09:49 +0530

    modified

---
+++ forex.js
@@ -0,0 +1,84 @@
+
+// var nam={
+// first:"Suthiga",
+// last:"Raja",
+// fullName : function(){
+//   return  this.first+this.last;
+// }
+// };
+// alert(nam.fullName());
+
+//
+// var x=100;
+// function add()
+// {
+//  z=x+y;
+//    alert(z);
+// }
+   i=1;
+var mm=function()
+{
+   y=90;
+
+   x=100;
+  z=x-y;
+console.log(z);
+i=i+1;
+if(i<10){
+ setTimeout(mm,1000);
+ }
+}
+ var time=setTimeout(mm,1000);
+ clearTimeout(time);
+// add();
+// mm();
+
+// for(var i=0;i<names.length;i=i+1)
+// {
+//   alert(names[i]);
+// }
+// var names=[];
+// do
+// {
+//   a=names.pop();
+//   alert(a);
+// }while(names.length>0)
+// alert("loop failed");
+//
+// var emp={
+//   first:"hi",
+//   last:"hello"
+// }
+// var emp1=emp;
+// emp.last=" man";
+// console.log(emp);
+// alert(emp.first+emp.last);
+// alert(emp1.first+emp1.last);
+// var a=1;
+// b=2;
+// delete this.a;
+// delete this.b;
+// console.log(a);
+// var hi="hiiii";
+// var abc=function(){
+//   var hi="hello";
+//   alert(hi);
+// }
+// abc();
+//
+// alert(location.href);
+// if(confirm("How is it?"))
+// {
+//   location="https://google.com";
+
+// x=10;
+// function add(){
+//   y=90;
+//   z=x+y;
+//   console.log(z);
+//   var x=20;
+// }
+// add();
+// var ex=document.getElementById("foo");
+// ex.style.color="red";
+// ex.style.backgroundColor="blue";
